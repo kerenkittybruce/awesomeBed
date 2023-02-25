aac481d3b8b0de92385415900465d13561c54752
Keren Bruce <kerenchelseabruce@gmail.com>
2023-02-25 11:49:16 +0200

    Model - index.js

--- model/index.js
+++ model/index.js
@@ -16,48 +16,51 @@ class User {
 
     // Login
 
-    login(req, res){
+    login(req, res) {
         const {EmailAdd, UserPassword} = req.body;
-        const strQry =
+        const strQry = 
         `
-        SELECT userID, FirstName, LastName, Gender, EmailAdd, UserPassword, UserRole, UserProfile
-        FROM users
-        WHERE EmailAdd = ${EmailAdd};
-        `
-
-        db.query(strQry, (err, data) => {
-            if (err) throw err;
-            if ((!data || (data == null)){
-                res.status(401).json({ err: "You provided an incorrect email address ."})
-            })
-            else (
-                await compare(UserPassword,
-                     data[0].UserPassword,
-                     (cErr, cResult) => {
-                        if (cErr) throw err;
-                        //  Create Token
-                        const jwToken = createToken({
-                            EmailAdd, UserPassword
-                        });
-                        // Create Cookie
-                        res.cookie('LegitUser', jwToken, {
-                            maxAge : 3600000,
+        SELECT FirstName, LastName, Gender, EmailAdd, UserPassword, UserRole, UserProfile
+        FROM Users
+        WHERE EmailAdd = '${EmailAdd}';
+        `;
+        db.query(strQry, async (err, data)=>{
+            if(err) throw err;
+            if((!data.length) || (data == null)) {
+                res.status(401).json({err: 
+                    "You provide a wrong email address"});
+            }else {
+                await compare(UserPassword, 
+                    data[0].UserPassword, 
+                    (cErr, cResult)=> {
+                        if(cErr) throw cErr;
+                        // Create a token
+                        const jwToken = 
+                        createToken(
+                            {
+                                EmailAdd, UserPassword 
+                            }
+                        );
+                        // Saving
+                        res.cookie('LegitUser',
+                        jwToken, {
+                            maxAge: 3600000,
                             httpOnly: true
                         })
-                        if (cResult){
+                        if(cResult) {
                             res.status(200).json({
                                 msg: 'Logged in',
                                 jwToken,
                                 result: data[0]
                             })
-                        } else {
+                        }else {
                             res.status(401).json({
-                                err: "You entered an invalid password or did not register ."
+                                err: 'You entered an invalid password or did not register. '
                             })
                         }
-                     });
-            };
-        })
+                    })
+            }
+        })     
     }
 
     // Fetch all users
@@ -149,6 +152,80 @@ class User {
 
     // Delete User
 
+    deleteUser(req, res) {
+        const strQry = 
+        `
+        DELETE FROM Users
+        WHERE userID = ?;
+        `;
+        //db
+        db.query(strQry,[req.params.id], 
+            (err)=>{
+            if(err) throw err;
+            res.status(200).json( {msg: 
+                "A record was removed from a database"} );
+        })    
+    }
+}
+
+// Product
+class Product {
+    fetchProducts(req, res) {
+        const strQry = `SELECT id, ProdName, ProdDescription, 
+        levels, Price, ProdQuantity, ImgURL
+        FROM products;`;
+        db.query(strQry, (err, results)=> {
+            if(err) throw err;
+            res.status(200).json({results: results})
+        });
+    }
+    fetchProduct(req, res) {
+        const strQry = `SELECT id, ProdName, ProdDescription, 
+        levels, Price, ProdQuantity, ImgURL
+        FROM products
+        WHERE id = ?;`;
+        db.query(strQry, [req.params.id], (err, results)=> {
+            if(err) throw err;
+            res.status(200).json({results: results})
+        });
+
+    }
+    addProduct(req, res) {
+        const strQry = 
+        `
+        INSERT INTO Products
+        SET ?;
+        `;
+        db.query(strQry,[req.body],
+            (err)=> {
+                if(err){
+                    res.status(400).json({err: "Unable to insert a new record."});
+                }else {
+                    res.status(200).json({msg: "Product saved"});
+                }
+            }
+        );    
+
+    }
+    updateProduct(req, res) {
+        const strQry = 
+        `
+        UPDATE Products
+        SET ?
+        WHERE id = ?
+        `;
+        db.query(strQry,[req.body, req.params.id],
+            (err)=> {
+                if(err){
+                    res.status(400).json({err: "Unable to update a record."});
+                }else {
+                    res.status(200).json({msg: "Product updated"});
+                }
+            }
+        );    
+
+    }
+
     deleteProduct(req, res) {
         const strQry = 
         `
@@ -160,11 +237,11 @@ class User {
             res.status(200).json({msg: "A product was deleted."});
         })
     }
+}
 
-    // Export User class
-    module.exports = {
-        User, 
-        Product
-    };
+// Export User class
 
+module.exports = {
+    User, 
+    Product
 }
